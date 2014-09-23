48a9745bfb6e0f0050ed187e57309a0ba03fa639
Ackinty Strappa <ackinty@gmail.com>
2014-09-24 01:49:57 +0200

    (Very)quirk n dirty firts tests.

--- controller/pagecontroller.php
+++ controller/pagecontroller.php
@@ -14,6 +14,11 @@ use \OCP\AppFramework\Controller;
 use \OCP\IRequest;
 use \OCP\IL10N;
 
+if(!class_exists('\SimplePie')) {
+    require_once __DIR__ . '/../3rdparty/simplepie/autoloader.php';
+}
+
+
 class PageController extends Controller {
 
     protected $trans;
@@ -32,7 +37,48 @@ class PageController extends Controller {
      * @NoCSRFRequired
      */
     public function index() {
-        return $this->render('main');
+        $feed = new \SimplePie();
+        $feed->set_feed_url('http://conceptships.blogspot.com/feeds/posts/default');
+        $feed->init();
+        $feed->handle_content_type();
+
+        $images = array();
+
+        foreach ($feed->get_items(0,2) as $item) {
+            $link = $siteLink = $description = '';
+            $description = $item->get_description();
+
+            $id = $item->get_id();
+            $anchor = substr($id, strpos($id, 'post-')+5);
+
+            $siteLink = $item->get_link() . '#' . $anchor ;
+
+            // start $imgLink = conceptshipsParse($description());
+            $imgLink = array('empty.jpg') ;
+            preg_match_all('%<img src="([^"]*)"[^>]*>%', $description, $matches) ;
+            if (!empty($matches)) {
+                $imgLink =  $matches[1];
+            }
+            // end $imgLink = conceptshipsParse($description());
+
+            if (!empty($imgLink)) {
+                foreach($imgLink as $imgSrc) {
+                    array_push($images, array(
+                        'siteLink' => $siteLink,
+                        'imgTitle' => $item->get_title(),
+                        'imgSrc' => $imgSrc,
+                        'itemId' => $item->get_id(),
+                    ));
+                }
+            }
+        }
+
+        $feed->__destruct();
+        unset($feed) ;
+
+        return $this->render('main', array(
+            'images' => $images,
+        ));
     }
 
 }
--- templates/main.php
+++ templates/main.php
@@ -22,7 +22,19 @@
 </div>
 
 <div id="container">
-
+    <?php foreach($_['images'] as $image) { ?>
+    <div class="blockImg">
+        <div class="feedImg">
+            <a href="<?php echo $image['siteLink']; ?>" title="<?php echo $image['imgTitle']; ?>">
+                <img src="<?php echo $image['imgSrc']; ?>" height="400" width="400">
+            </a>
+        </div>
+        <div class="action">
+            <a href="<?php echo $image['itemId']; ?>" title="Lien vers le post d'origine">Post</a><br>
+            <a href="">EE</a>
+        </div>
+    </div>
+    <?php } ?>
 </div>
 
 <div id="footer">
